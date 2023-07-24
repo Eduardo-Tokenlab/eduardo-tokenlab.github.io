80054754c74f608a866e81aeb6621a7521223e88
Eduardo-Tokenlab <136387272+Eduardo-Tokenlab@users.noreply.github.com>
2023-07-24 10:36:32 -0300

    Add files via upload

--- su/src/script.js
+++ su/src/script.js
@@ -1,4 +1,39 @@
-function myFunction() {
-    var element = document.getElementById("collapse");
-    element.classList.toggle("active");
+function SuNavHide() {
+    var element = document.body.classList.toggle('active');
+    var elements = document.querySelectorAll('*');
+    elements.forEach((element) => {
+        element.classList.remove('open');
+    });
+}
+function SuNavOpen() {
+    var element = document.body.classList.add('active');
+    event.target.classList.add('open');
+}
+function SuNavUser() {
+    var element = document.getElementById('user');
+    element.classList.add('open');
+}
+function SuNavSearch() {
+    var element = document.getElementById('search');
+    element.classList.add('open');
+}
+function SuNavRamos() {
+    var element = document.getElementById('ramos');
+    element.classList.add('open');
+}
+function SuNavPrev() {
+    var element = document.getElementById('prev');
+    element.classList.add('open');
+}
+function SuNavSaude() {
+    var element = document.getElementById('saude');
+    element.classList.add('open');
+}
+function SuNavVida() {
+    var element = document.getElementById('vida');
+    element.classList.add('open');
+}
+function SuNavOdonto() {
+    var element = document.getElementById('odonto');
+    element.classList.add('open');
 }
